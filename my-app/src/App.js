a69687368fd6d788951a96ae0bd9c17fa3e1c161
nicole <nicolegru25@gmail.com>
2021-12-10 22:41:30 +0200

    Fixed positioning and scores

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,8 @@
-import logo from './logo.svg';
 import bigabidLogo from './bigabid-logo.png'
 import './App.css';
 import { useState, useEffect } from 'react';
-import BasicTable, {error, lose} from './Table'
+import BasicTable, {error, lose, win, resetStats} from './Table'
 import BasicList from "./List";
-import { win } from './Table'
-import {IconButton, List, ListItem, ListItemText} from "@mui/material";
 
 
 function App() {
@@ -16,6 +13,7 @@ function App() {
   const [endTime, setEndTime] = useState(0);
 
   const handleSelect = async (e) => {
+    resetStats()
     setSelected(e.target.value);
     if (selected !== "Campaign") {
       setTime(Math.round(new Date().getTime() / 1000));
@@ -37,14 +35,11 @@ function App() {
       setBids("");
     }
     else if (selected && selected !== "Campaign") {
-      //setBids("");
-      console.log(selected, time, endTime);
       const interval = setInterval(() => {
         setEndTime(endTime + 1);
         getBids(selected, time);
       }, 1000);
       return () => {
-        console.log("cleared");
         clearInterval(interval);
       }
     }
@@ -52,32 +47,12 @@ function App() {
 
   useEffect(() => {
     if (endTime) {
-      //
     }
   }, [endTime]);
 
   async function getBids(select, startTime) {
-    let allData = "";
     const response = await fetch("http://localhost:3001/list_of_bids?startTime=" + startTime + "&endTime=" + endTime);
     const data = await response.json();
-
-    // for (let index in data) {
-    //   if (index % 2 === 0) {
-    //     let m = await getBid(data[index]);
-    //     let j = JSON.parse(m);
-    //
-    //     if (j.campaign === select) {
-    //       console.log(j.status);
-    //
-    //       if (j.status === 0) {
-    //         allData += m;
-    //       }
-    //       if (j.status === 1) {
-    //         setWin(win + 1);
-    //       }
-    //     }
-    //   }
-    // }
     setBids(data);
   }
 
@@ -85,12 +60,6 @@ function App() {
   return (
       <div className="App">
         <header className="App-header">
-        {/*  image: {*/}
-        {/*  flex: 1,*/}
-        {/*  aspectRatio: 1.5,*/}
-        {/*  resizeMode: 'contain',*/}
-
-        {/*}*/}
           <img src={bigabidLogo} />
           Bigabid Banker
           {campaigns && (
@@ -102,24 +71,29 @@ function App() {
               </select>
 
             )}
-          <div  style={{'margin-left': '100px',
-          'align-self': 'flex-start',
-          'margin-top': '50px'}}>
-            Pending bids
-            <BasicTable bids={bids} selected={selected}/>
+         <br/>
+          <div class="flexbox-container">
+
+
+            <div
+                style={{'margin-right': '30vw',
+                'margin-left': '5vw'}}
+            >
+              Pending bids
+              <BasicTable bids={bids} selected={selected}/>
+            </div>
+
+            <div
+                style={{'margin-right': '5vw'}}
+            >
+              Resolved bids
+              <BasicList wins={win} loses={lose} errors={error}>
+
+              </BasicList>
+            </div>
+
           </div>
 
-          <div  style={{'margin-right': '100px',
-            'align-self': 'flex-end',
-            'margin-top': '-252px'}}>
-            Resolved bids
-            <BasicList wins={win} loses={lose} errors={error}>
-
-            </BasicList>
-          </div>
-
-          <p>{ selected }</p>
-          <p></p>
         </header>
       </div>
   );
--- my-app/src/Table.js
+++ my-app/src/Table.js
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import {useEffect, useState} from 'react';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -6,12 +7,8 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-import { useState, useEffect } from 'react';
 
 
-function createData(id, time, price) {
-    return { id, time, price };
-}
 export let win = 0;
 export let lose = 0;
 export let error = 0
@@ -27,23 +24,23 @@ function setErrors(value) {
 }
 
 function convertTime(timestamp) {
-    return new Date(timestamp * 1000); //check its correct
+    return new Date(timestamp * 1000);
 }
 
-export default function BasicTable({bids, selected, campaign}) {
+export function resetStats() {
+    win = 0;
+    error = 0;
+    lose = 0;
+}
+
+export default function BasicTable({bids, selected}) {
     const [table, setTable] = useState([]);
-    // const [win, setWin] = useState(0);
-    // const [error, setError] = useState(0);
-    // const [lose, setLose] = useState(0);
 
     async function getBidInfo(bids, campaign) {
-        win = 0;
-        lose = 0;
-        error = 0;
         const result = [];
         if (bids) {
             for (let i = 0; i < bids.length; i++) {
-                if (i % 2 !== 0) { // all bids are even and timestamps are odd
+                if ((i & 1) === 1) { // all bids are even and timestamps are odd
                     let date = convertTime(bids[i]);
                     let bidId = bids[i - 1];
                     let bidInfo = await getBid(bidId);
@@ -59,15 +56,12 @@ export default function BasicTable({bids, selected, campaign}) {
                             });
                         }
                         else if (bidInfoJson.status === 1) {
-                            // setError(error + 1);
                             setErrors(1);
                         }
                         else if (bidInfoJson.status === 2) {
-                            // setLose(lose + 1);
                             setLoses(1);
                         }
                         else {
-                            // setWin(win + 1);
                             setWins(1);
                         }
                     }
@@ -81,27 +75,24 @@ export default function BasicTable({bids, selected, campaign}) {
 
     async function getBid(bidId) {
         const response = await fetch("http://localhost:3001/bid/" + bidId);
-        const data = await response.json();
-        return data;
+        return await response.json();
     }
 
     useEffect(async () => {
         if (selected) {
-            //setTable(await getBidInfo(bids));
             console.log(bids);
-            //const j = await getBidInfo(bids);
             setTable(await getBidInfo(bids, selected));
             console.log(table);
         }
     }, [bids]);
 
     return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 300 }} aria-label="simple table">
+        <TableContainer component={Paper} style={{maxHeight: '40vh'}}>
+            <Table stickyHeader sx={{ minWidth: 300, overflow: 'auto' }} aria-label="simple table">
                 <TableHead>
                     <TableRow>
                         <TableCell>Bid Id</TableCell>
-                        <TableCell align="right">Bid Time</TableCell>
+                        <TableCell align="center">Bid Time</TableCell>
                         <TableCell align="right">Price&nbsp;($)</TableCell>
                     </TableRow>
                 </TableHead>
